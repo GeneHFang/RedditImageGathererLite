a449656d86a76d52b445b2ac2022e4d4a58e260f
GeneHFang <gene.li@nyu.edu>
2020-05-11 20:51:02 -0400

    create two cases based on actions and added two default values

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,6 +1,11 @@
 //default values
 const defaultState = {
 
+    //String subreddit title. Default subreddit is r/all
+    subreddit: "all",
+
+    //Array of image objects containing info from first 25 image/gif/video posts 
+    images: [],
 }
 
 //Reducer
@@ -9,6 +14,16 @@ const reducer = (prevState = defaultState, action) => {
         //case format example
         //case "EX":
         //  return {...prevState, exampleKey: action.payload}
+
+        //Change subreddit to 
+        case "CHANGE_SUBREDDIT":
+            return {...prevState, subreddit: action.payload.subredditName};
+        
+        //Update the array of images
+        case "UPDATE_ARRAY":
+            return {...prevState, images: action.payload.images}
+
+        //default case
         default:
             return prevState;
     };
